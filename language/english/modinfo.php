7e4f3948a72260506e231cbe10e6f43adc282c9a
geekwright <richard@geekwright.com>
2013-11-08 22:00:55 -0600

    Establish dummy module

---
+++ language/english/modinfo.php
@@ -0,0 +1,7 @@
+<?php
+define('_MI_DUMMY_ADMENU1', "Home");
+define('_MI_DUMMY_ADMENU2', "About");
+define('_MI_DUMMY_CONFIG1', "Config 1");
+define('_MI_DUMMY_CONFIG1_DSC', "Config 1 Description");
+define('_MI_DUMMY_CONFIG2', "Conf 2");
+define('_MI_DUMMY_CONFIG2_DSC', "Conf 2 Desc");
